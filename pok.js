671173ed3f78e07408d0e46d7a1ca51db0d8c4e8
marszallek <marszallek@vp.pl>
2018-11-30 22:04:00 +0100

    colors done

--- pok.js
+++ pok.js
@@ -1,13 +1,13 @@
 
-    var one = Math.round(Math.random() * 24 + 1);
-    var two = Math.round(Math.random() * 24 + 1);
-    var three = Math.round(Math.random() * 24 + 1);
-    var four = Math.round(Math.random() * 24 + 1);
-    var five = Math.round(Math.random() * 24 + 1);
+    var one = Math.round(Math.random() * 23 + 1);
+    var two = Math.round(Math.random() * 23 + 1);
+    var three = Math.round(Math.random() * 23 + 1);
+    var four = Math.round(Math.random() * 23 + 1);
+    var five = Math.round(Math.random() * 23 + 1);
 	
 function poker(){
-    if (one == 1 ){
-        one = '9' + 'spade';
+    if ( one === 1){
+            one = '9' + 'spade';
     }
         else if (one == 2){
         one = '9' + 'diamond';
@@ -19,81 +19,357 @@ function poker(){
         one = '9' + 'heart';
     }
         else if (one == 5){
-        one = 'K';
+        one = '10' + 'spade';
     }
-        else one = 'A';
+        else if (one == 6){
+        one = '10' + 'diamond';
+    }
+        else if (one == 7){
+        one = '10' + 'club';
+    }
+        else if (one == 8){
+        one = '10' + 'heart';
+    }
+        else if (one == 9){
+        one = 'J' + 'spade';
+    }
+        else if (one == 10){
+        one = 'J' + 'diamond';
+    }
+        else if (one == 11){
+        one = 'J' + 'club';
+    }
+        else if (one == 12){
+        one = 'J' + 'heart';
+    }
+        else if (one == 13){
+        one = 'Q' + 'spade';
+    }
+        else if (one == 14){
+        one = 'Q' + 'diamond';
+    }
+        else if (one == 15){
+        one = 'Q' + 'club';
+    }
+        else if (one == 16){
+        one = 'Q' + 'heart';
+    }   
+        else if (one == 17){
+        one = 'K' + 'spade';
+    }
+        else if (one == 18){
+        one = 'K' + 'diamond';
+    }
+        else if (one == 19){
+        one = 'K' + 'club';
+    }
+        else if (one == 20){
+        one = 'K' + 'heart';
+    }    
+        else if (one == 21){
+        one = 'A' + 'spade';
+    }
+        else if (one == 22){
+        one = 'A' + 'diamond';
+    }
+        else if (one == 23){
+        one = 'A' + 'club';
+    }
+        else if (one == 24){
+        one = 'A' + 'heart';
+    }
 
-        
     if (two == 1 ){
-        two = "9";
+        two = '9' + 'spade';
     }
         else if (two == 2){
-        two = '10';
+        two = '9' + 'diamond';
     }
         else if (two == 3){
-        two = 'J';
+        two = '9' + 'club';
     }
         else if (two == 4){
-        two = 'Q';
+        two = '9' + 'heart';
     }
         else if (two == 5){
-        two = 'K';
+        two = '10' + 'spade';
     }
-        else two = 'A';
-
+        else if (two == 6){
+        two = '10' + 'diamond';
+    }
+        else if (two == 7){
+        two = '10' + 'club';
+    }
+        else if (two == 8){
+        two = '10' + 'heart';
+    }
+        else if (two == 9){
+        two = 'J' + 'spade';
+    }
+        else if (two == 10){
+        two = 'J' + 'diamond';
+    }
+        else if (two == 11){
+        two = 'J' + 'club';
+    }
+        else if (two == 12){
+        two = 'J' + 'heart';
+    }
+        else if (two == 13){
+        two = 'Q' + 'spade';
+    }
+        else if (two == 14){
+        two = 'Q' + 'diamond';
+    }
+        else if (two == 15){
+        two = 'Q' + 'club';
+    }
+        else if (two == 16){
+        two = 'Q' + 'heart';
+    }   
+        else if (two == 17){
+        two = 'K' + 'spade';
+    }
+        else if (two == 18){
+        two = 'K' + 'diamond';
+    }
+        else if (two == 19){
+        two = 'K' + 'club';
+    }
+        else if (two == 20){
+        two = 'K' + 'heart';
+    }    
+        else if (two == 21){
+        two = 'A' + 'spade';
+    }
+        else if (two == 22){
+        two = 'A' + 'diamond';
+    }
+        else if (two == 23){
+        two = 'A' + 'club';
+    }
+        else if (two == 24){
+        two = 'A' + 'heart';
+    }
 
     if (three == 1 ){
-        three = "9";
+        three = '9' + 'spade';
     }
         else if (three == 2){
-        three = '10';
+        three = '9' + 'diamond';
     }
         else if (three == 3){
-        three = 'J';
+        three = '9' + 'club';
     }
         else if (three == 4){
-        three = 'Q';
+        three = '9' + 'heart';
     }
         else if (three == 5){
-        three = 'K';
+        three = '10' + 'spade';
     }
-        else three = 'A';
-    
+        else if (three == 6){
+        three = '10' + 'diamond';
+    }
+        else if (three == 7){
+        three = '10' + 'club';
+    }
+        else if (three == 8){
+        three = '10' + 'heart';
+    }
+        else if (three == 9){
+        three = 'J' + 'spade';
+    }
+        else if (three == 10){
+        three = 'J' + 'diamond';
+    }
+        else if (three == 11){
+        three = 'J' + 'club';
+    }
+        else if (three == 12){
+        three = 'J' + 'heart';
+    }
+        else if (three == 13){
+        three = 'Q' + 'spade';
+    }
+        else if (three == 14){
+        three = 'Q' + 'diamond';
+    }
+        else if (three == 15){
+        three = 'Q' + 'club';
+    }
+        else if (three == 16){
+        three = 'Q' + 'heart';
+    }   
+        else if (three == 17){
+        three = 'K' + 'spade';
+    }
+        else if (three == 18){
+        three = 'K' + 'diamond';
+    }
+        else if (three == 19){
+        three = 'K' + 'club';
+    }
+        else if (three == 20){
+        three = 'K' + 'heart';
+    }    
+        else if (three == 21){
+        three = 'A' + 'spade';
+    }
+        else if (three == 22){
+        three = 'A' + 'diamond';
+    }
+        else if (three == 23){
+        three = 'A' + 'club';
+    }
+        else if (three == 24){
+        three = 'A' + 'heart';
+    }
 
     if (four == 1 ){
-        four = "9";
+        four = '9' + 'spade';
     }
         else if (four == 2){
-        four = '10';
+        four = '9' + 'diamond';
     }
         else if (four == 3){
-        four = 'J';
+        four = '9' + 'club';
     }
         else if (four == 4){
-        four = 'Q';
+        four = '9' + 'heart';
     }
         else if (four == 5){
-        four = 'K';
+        four = '10' + 'spade';
     }
-        else four = 'A';
-
+        else if (four == 6){
+        four = '10' + 'diamond';
+    }
+        else if (four == 7){
+        four = '10' + 'club';
+    }
+        else if (four == 8){
+        four = '10' + 'heart';
+    }
+        else if (four == 9){
+        four = 'J' + 'spade';
+    }
+        else if (four == 10){
+        four = 'J' + 'diamond';
+    }
+        else if (four == 11){
+        four = 'J' + 'club';
+    }
+        else if (four == 12){
+        four = 'J' + 'heart';
+    }
+        else if (four == 13){
+        four = 'Q' + 'spade';
+    }
+        else if (four == 14){
+        four = 'Q' + 'diamond';
+    }
+        else if (four == 15){
+        four = 'Q' + 'club';
+    }
+        else if (four == 16){
+        four = 'Q' + 'heart';
+    }   
+        else if (four == 17){
+        four = 'K' + 'spade';
+    }
+        else if (four == 18){
+        four = 'K' + 'diamond';
+    }
+        else if (four == 19){
+        four = 'K' + 'club';
+    }
+        else if (four == 20){
+        four = 'K' + 'heart';
+    }    
+        else if (four == 21){
+        four = 'A' + 'spade';
+    }
+        else if (four == 22){
+        four = 'A' + 'diamond';
+    }
+        else if (four == 23){
+        four = 'A' + 'club';
+    }
+        else if (four == 24){
+        four = 'A' + 'heart';
+    }
 
     if (five == 1 ){
-        five = "9";
+        five = '9' + 'spade';
     }
         else if (five == 2){
-        five = '10';
+        five = '9' + 'diamond';
     }
         else if (five == 3){
-        five = 'J';
+        five = '9' + 'club';
     }
         else if (five == 4){
-        five = 'Q';
+        five = '9' + 'heart';
     }
         else if (five == 5){
-        five = 'K';
+        five = '10' + 'spade';
     }
-        else five = 'A';
+        else if (five == 6){
+        five = '10' + 'diamond';
+    }
+        else if (five == 7){
+        five = '10' + 'club';
+    }
+        else if (five == 8){
+        five = '10' + 'heart';
+    }
+        else if (five == 9){
+        five = 'J' + 'spade';
+    }
+        else if (five == 10){
+        five = 'J' + 'diamond';
+    }
+        else if (five == 11){
+        five = 'J' + 'club';
+    }
+        else if (five == 12){
+        five = 'J' + 'heart';
+    }
+        else if (five == 13){
+        five = 'Q' + 'spade';
+    }
+        else if (five == 14){
+        five = 'Q' + 'diamond';
+    }
+        else if (five == 15){
+        five = 'Q' + 'club';
+    }
+        else if (five == 16){
+        five = 'Q' + 'heart';
+    }   
+        else if (five == 17){
+        five = 'K' + 'spade';
+    }
+        else if (five == 18){
+        five = 'K' + 'diamond';
+    }
+        else if (five == 19){
+        five = 'K' + 'club';
+    }
+        else if (five == 20){
+        five = 'K' + 'heart';
+    }    
+        else if (five == 21){
+        five = 'A' + 'spade';
+    }
+        else if (five == 22){
+        five = 'A' + 'diamond';
+    }
+        else if (five == 23){
+        five = 'A' + 'club';
+    }
+        else if (five == 24){
+        five = 'A' + 'heart';
+    }
     
         var my = [one, two, three, four, five];
 		document.getElementById("pok1").innerHTML = my.join(' ');
